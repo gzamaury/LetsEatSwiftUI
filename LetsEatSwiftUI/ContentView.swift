9c6e027aebeda017409caffe45e3e7cdcb474aaa
AmauryGz <gzamaury@hotmail.com>
2021-03-18 17:17:08 -0600

    Composing the Restaurant Detail screen

--- LetsEatSwiftUI/ContentView.swift
+++ LetsEatSwiftUI/ContentView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("The Tap Trailhouse")
-                .font(.title)
-            HStack {
-                Text("17 Union St")
-                    .font(.subheadline)
-                Spacer()
-                Text("Boston")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("The Tap Trailhouse")
+                    .font(.title)
+                HStack {
+                    Text("17 Union St")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Boston")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            Spacer()
         }
-    .padding()
     }
 }
 
