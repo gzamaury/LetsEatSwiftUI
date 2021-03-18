0796d9f51425bc806b507aee905b190261b0eb90
AmauryGz <gzamaury@hotmail.com>
2021-03-17 19:19:34 -0600

    Creating a SwiftUI project

---
+++ LetsEatSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsEatSwiftUI
+//
+//  Created by MacGza on 17/03/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Lets Eat")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
